bbf3ddea0a1e4248ffe6f5940076cb9008430512
ManUnited-reddevils <28774154+ManUnited-reddevils@users.noreply.github.com>
2018-09-05 21:53:27 -0400

    Songming added readme.p5.txt

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("vlieng3");
           //Person5 replace P5 with your name
           //and gburdell5 with your gt id
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("gburdell5");
+          Person5 p5 = new Person5("Songming Liu");
+          toPrint += p5.toString("sliu607");
           System.out.println(toPrint);
 
 
